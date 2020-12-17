ca6642c304ebe0e91e358bb05a47fc61e112d676
Simon Panek <paneks19@gmail.com>
2020-12-16 20:01:45 -0800

    all routes working

--- src/serverFinal.js
+++ src/serverFinal.js
@@ -11,6 +11,7 @@ const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./routes/routesAuth.js');
 const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
 const logger = require('./middleware/logger.js');
 
 
@@ -27,6 +28,7 @@ app.use(logger);
 app.use(authRoutes);
 
 app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes)
 
 // Catchalls
 app.use('*', notFound);
