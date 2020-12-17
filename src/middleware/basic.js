05062f86e5c86d29b6fe21520bbf5f378d0ba4ab
Simon Panek <paneks19@gmail.com>
2020-12-16 21:46:27 -0800

    all route testing passing

--- src/middleware/basic.js
+++ src/middleware/basic.js
@@ -4,15 +4,10 @@ const base64 = require('base-64');
 const User = require('../models/users.js');
 
 module.exports = async (req, res, next) => {
-  try {
-
     if (!req.headers.authorization) { return _authError(); }
     
     let basic = req.headers.authorization.split(' ').pop();
     let [user, pass] = base64.decode(basic).split(':');
-  } catch (e) {
-    throw new Error(e.message)
-  }
 
   try {
     req.user = await User.authenticateBasic(user, pass)
--- src/routes/v1.js
+++ src/routes/v1.js
@@ -37,50 +37,50 @@ async function handleGetAll(req, res) {
   try {
     let allRecords = await req.model.get();
     res.status(200).json(allRecords);
-  } catch (e) {
-    throw new Error(e.message)
-  }
+ } catch (e) {
+   throw new Error(e.message)
+ }
 }
 
 async function handleGetOne(req, res) {
-  try {
+ try {
     const id = req.params.id;
     let theRecord = await req.model.get(id)
     res.status(200).json(theRecord);
-  } catch (e) {
-    throw new Error(e.message)
-  }
+ } catch (e) {
+   throw new Error(e.message)
+ }
 }
 
 async function handleCreate(req, res) {
-  try {
+ try {
     let obj = req.body;
     let newRecord = await req.model.create(obj);
     res.status(201).json(newRecord);
-  } catch (e) {
-    throw new Error(e.message)
-  }
+ } catch (e) {
+   throw new Error(e.message)
+ }
 }
 
 async function handleUpdate(req, res) {
-  try {
+ try {
     const id = req.params.id;
     const obj = req.body;
     let updatedRecord = await req.model.update(id, obj)
     res.status(200).json(updatedRecord);
-  } catch (e) {
-    throw new Error(e.message)
-  }
+ } catch (e) {
+   throw new Error(e.message)
+ }
 }
 
 async function handleDelete(req, res) {
-  try {
+ try {
     let id = req.params.id;
     let deletedRecord = await req.model.delete(id);
     res.status(200).json(deletedRecord);
-  } catch (e) {
-    throw new Error(e.message)
-  }
+ } catch (e) {
+   throw new Error(e.message)
+ }
 }
 
 
